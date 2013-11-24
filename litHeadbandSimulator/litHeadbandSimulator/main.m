e4cc165a98abc59a338d719bec2aab137fbb0690
ryantuck <ryanstephentuck@gmail.com>
2013-11-23 21:04:24 -0500

    First and hopefully only commit
    Fuck github and VCS

---
+++ litHeadbandSimulator/litHeadbandSimulator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  litHeadbandSimulator
+//
+//  Created by Ryan Tuck on 11/23/13.
+//  Copyright (c) 2013 Ryan Tuck. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
---
+++ litHeadbandSimulator/litHeadbandSimulatorTests/litHeadbandSimulatorTests.m
@@ -0,0 +1,34 @@
+//
+//  litHeadbandSimulatorTests.m
+//  litHeadbandSimulatorTests
+//
+//  Created by Ryan Tuck on 11/23/13.
+//  Copyright (c) 2013 Ryan Tuck. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface litHeadbandSimulatorTests : XCTestCase
+
+@end
+
+@implementation litHeadbandSimulatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
